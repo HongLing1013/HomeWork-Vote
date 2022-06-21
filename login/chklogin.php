a66f2373cc96e245e51702386918143f042e7a96
Hollie <tsubasababy1013@gmail.com>
2022-06-21 10:53:15 +0800

    完成檢查帳密是否正確的功能

--- login/chklogin.php
+++ login/chklogin.php
@@ -1,9 +1,20 @@
 <?php
+include "connect.php";//連線資料庫
 
-// 檢查帳密
-if(true){
+$acc=$_POST['acc'];
+$pw=$_POST['pw'];//接收帳號密碼
+
+$sql="SELECT * FROM `users` WHERE `acc`='$acc' && `pw`='$pw'";//尋找資料表的acc跟pw是否相符
+$user=$pdo->query($sql)->fetch();
+
+$error='';
+
+if($acc==$users['acc'] && $pw==$users['pw']){
+  // 登入成功
   header("location:member_center.php");
 }else{
-  header("location:login.php");
+  // 登入失敗
+  $error="帳號密碼錯誤";
+  header("location:login.php?error=$error");
 }
 ?>
