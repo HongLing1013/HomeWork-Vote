afdc93b86f3bd6aaabdd9c221c20288864300e90
Hollie <tsubasababy1013@gmail.com>
2022-06-21 21:29:24 +0800

    建立動態載入頁面機制

--- back.php
+++ back.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,20 +8,33 @@
   <title>投票管理中心</title>
   <link rel="stylesheet" href="./css/back.css">
 </head>
+
 <body>
   <!-- 上方選單 -->
   <nav>
-    <?php include "./layout/header.php";?>
-    <?php include "./layout/back_nav.php";?>
+    <?php include "./layout/header.php"; ?>
+    <?php include "./layout/back_nav.php"; ?>
   </nav>
 
   <!-- 主要內容 -->
   <div class="container">
     <h1>投票管理中心</h1>
+    <?php
+    if (isset($_GET['do'])) { //如果有取得do這個頁面的話執行
+      $file = "./back/" . $_GET['do'] . ".php";
+      include $file;
+    } else {
+    ?>
+      <button class=btn onclick="location.href='?do=add_vote'">新增投票</button><!-- get傳值檔案名稱 -->
+      <div>投票列表</div>
+    <?php
+    }
+    ?>
 
   </div>
 
   <!-- 頁尾 -->
-  <?php include "./layout/footer.php";?>
+  <?php include "./layout/footer.php"; ?>
 </body>
+
 </html>
---
+++ back/add_vote.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>新增投票</title>
+</head>
+<body>
+  <!-- 直接帶入框框裡面 所以不用再link一次整個頁面 -->
+  新增投票的表單
+</body>
+</html>
