056c548402ec6ebdd64ad84f38a763c13832f0e9
Hollie <tsubasababy1013@gmail.com>
2022-06-21 11:04:28 +0800

    簡化登入判斷

--- login/chklogin.php
+++ login/chklogin.php
@@ -4,17 +4,15 @@ include "connect.php";//連線資料庫
 $acc=$_POST['acc'];
 $pw=$_POST['pw'];//接收帳號密碼
 
-$sql="SELECT * FROM `users` WHERE `acc`='$acc' && `pw`='$pw'";//尋找資料表的acc跟pw是否相符
-$user=$pdo->query($sql)->fetch();
+$sql="SELECT count(*) FROM `users` WHERE `acc`='$acc' && `pw`='$pw'";//尋找資料表的acc跟pw是否相符
+$chk=$pdo->query($sql)->fetchColumn();
 
 $error='';
 
-if($acc==$users['acc'] && $pw==$users['pw']){
-  // 登入成功
-  header("location:member_center.php");
+if($chk){//如果資料庫有這一筆資料的話就是Ture
+  header("location:member_center.php");// 登入成功導向會員頁
 }else{
-  // 登入失敗
   $error="帳號密碼錯誤";
-  header("location:login.php?error=$error");
+  header("location:login.php?error=$error");// 登入失敗回到登入頁
 }
 ?>
