a176f2d0bec1013d0a38de488ab4ff0085524748
Hollie <tsubasababy1013@gmail.com>
2022-06-22 17:53:33 +0800

    讓編輯頁面連線資料庫取資料

--- back/edit.php
+++ back/edit.php
@@ -1,5 +1,9 @@
 <?php
-
-include_once "../api/base.php";
-
+$id=$_GET['id'];//取得要編輯的序號
+$row=find('subjects',$id);//取得要編輯的內容
+$opts=all('options',['subject_id'=>$id]);
+dd($row);
+dd($opts);
 ?>
+
+編輯<?=$id;?>
