5ba69383d4e7b76fd24dd247269c0d9679b6b464
Hollie <tsubasababy1013@gmail.com>
2022-06-23 22:41:41 +0800

    將後臺的投票列表狀態完整顯示與加上CSS

--- back.php
+++ back.php
@@ -35,14 +35,47 @@
 
       <div>
         <ul>
-
+          <li class="list-header">
+            <div>投票主題：</div>
+            <div>單/複選題：</div>
+            <div>投票期間：</div>
+            <div>剩餘天數：</div>
+            <div>投票人數：</div>
+            <div>操作：</div>
+          </li>
           <?php
           $subjects=all('subjects'); //取得所有投票列表
-          foreach($subjects as $subject){
+          foreach($subjects as $subject){//使用迴圈印內容
             echo "<li class='list-items'>";
-            echo $subject['subject']; //只取得欄位
+            echo "<div>{$subject['subject']}</div>";//只取得欄位
+
+            if($subject['multiple']==0){
+              echo "<div class='text-center'>單選題</div>";
+            }else{
+              echo "<div class='text-center'>複選題</div>";
+            }
+
+            echo "<div class='text-center'>";//投票開始與結束時間
+            echo $subject['start']. "~" .$subject['end'];
+            echo "</div>";
+
+            echo "<div class='text-center'>";//投票剩餘天數
+              $today=strtotime("now");
+              $end=strtotime($subject['end']);
+              if(($end-$today)>0){//如果投票還在進行
+                $remain=floor(($end-$today)/(60*60*24));
+                echo "倒數".$remain."天結束";
+              }else{//如果投票已經截止
+                echo "<span style='color:grey;'>投票已截止</span>";
+              }
+            echo "</div>";
+
+            echo "<div class='text-center'>{$subject['total']}</div>";//投票總人數
+
+            echo "<div class='text-center'>";//操作區
             echo "<a class='edit' href='?do=edit&id={$subject['id']}'>編輯</a>";
             echo "<a class='del' href='?do=del&id={$subject['id']}'>刪除</a>";
+            echo "</div>";
             echo "</li>";
           }
           ?>
