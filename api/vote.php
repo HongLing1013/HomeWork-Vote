e78683b6634c8116bc92ba558f6f0c1e264271da
Hollie <tsubasababy1013@gmail.com>
2022-06-29 09:52:52 +0800

    投票顯示功能以及投票紀錄功能

---
+++ api/vote.php
@@ -0,0 +1,36 @@
+<?php
+// 紀錄投票結果
+include_once "base.php";
+
+if(isset($_POST['opt'])){
+
+if(is_array($_POST['opt'])){
+  foreach($_POST['opt'] as $key => $opt){
+    $option=find("options",$opt);
+    $option['total']++;
+    save("options",$option);
+    if($key==0){
+      $subject=find("subjects",$option['subject_id']);
+      $subject['total']++;
+      save("subjects",$subject);
+    }
+    $log=['user_id'=>(isset($_SESSION['user']))?$_SESSION['user']:0,
+          'subject_id'=>$subject['id'],
+          'option_id'=>$option['id']];
+    save("logs",$log);
+  }
+}else{
+    //單選題
+    $option=find("options",$_POST['opt']);
+    $option['total']++;
+    save("options",$option);
+    $subject=find("subjects",$option['subject_id']);
+    $subject['total']++;
+    save("subjects",$subject);
+    $log=['user_id'=>(isset($_SESSION['user']))?$_SESSION['user']:0,
+          'subject_id'=>$subject['id'],
+          'option_id'=>$option['id']];
+    save("logs",$log);
+}
+}
+to("../index.php");
--- front/vote.php
+++ front/vote.php
@@ -1,9 +1,35 @@
 <?php
 include_once "./api/base.php";
 
-$subject=find("subjects",$_GET['id']);
-$opts=all('options',['subject_id'=>$_GET['id']]);
+$subject = find("subjects", $_GET['id']);
+$opts = all('options', ['subject_id' => $_GET['id']]);
 
-dd($subject);
-dd($opts);
+// dd($subject);
+// dd($opts);
 ?>
+<h1><?= $subject['subject']; ?></h1>
+<form action="./api/vote.php" method="post">
+<?php
+foreach ($opts as $opt) {
+?>
+  <div class="vote-item">
+    <?php
+    if ($subject['multiple'] == 0) { //單選題
+    ?>
+      <input type="radio" name="opt" value="<?=$opt['id'];?>">
+    <?php
+    } else {//複選題
+    ?>
+      <input type="checkbox" name="opt[]" value="<?=$opt['id'];?>">
+    <?php
+    }
+    ?>
+    <?= $opt['option']; ?>
+  </div>
+<?php
+}
+?>
+<input type="submit" value="投票">
+<input type="reset" value="重置">
+<input type="button" value="放棄" onclick="location.href='index.php'">
+</form>
