7ee680efc65d1e02fa4253fe403cca3f55313162
Hollie <tsubasababy1013@gmail.com>
2022-06-29 16:49:04 +0800

    更新SQL資料庫

--- vote.sql
+++ vote.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2022-06-23 02:52:14
+-- 產生時間： 2022-06-29 09:42:46
 -- 伺服器版本： 10.4.24-MariaDB
 -- PHP 版本： 8.1.6
 
@@ -37,16 +37,30 @@ CREATE TABLE `admins` (
 -- --------------------------------------------------------
 
 --
--- 資料表結構 `log`
+-- 資料表結構 `logs`
 --
 
-CREATE TABLE `log` (
+CREATE TABLE `logs` (
   `id` int(11) UNSIGNED NOT NULL COMMENT '序號',
   `user_id` int(11) UNSIGNED NOT NULL COMMENT '投票者',
   `subject_id` int(11) UNSIGNED NOT NULL COMMENT '投票項目',
   `option_id` int(11) UNSIGNED NOT NULL COMMENT '選項',
   `vote_time` timestamp NOT NULL DEFAULT current_timestamp() COMMENT '投票時間'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `logs`
+--
+
+INSERT INTO `logs` (`id`, `user_id`, `subject_id`, `option_id`, `vote_time`) VALUES
+(1, 0, 5, 24, '2022-06-29 01:49:35'),
+(2, 0, 2, 1, '2022-06-29 01:59:16'),
+(3, 0, 2, 5, '2022-06-29 02:15:27'),
+(4, 0, 5, 23, '2022-06-29 04:01:36'),
+(5, 0, 5, 22, '2022-06-29 04:01:45'),
+(6, 0, 5, 22, '2022-06-29 04:01:52'),
+(7, 0, 6, 28, '2022-06-29 04:02:07'),
+(8, 0, 5, 22, '2022-06-29 04:44:14');
 
 -- --------------------------------------------------------
 
@@ -66,12 +80,47 @@ CREATE TABLE `options` (
 --
 
 INSERT INTO `options` (`id`, `option`, `subject_id`, `total`) VALUES
-(1, '美而美', 2, 0),
+(1, '美而美', 2, 1),
 (2, 'Qbuger', 2, 0),
 (3, '麥當勞', 2, 0),
 (4, 'MOS', 2, 0),
-(5, '豆漿店', 2, 0),
-(6, '大腸麵線', 2, 0);
+(5, '豆漿店', 2, 1),
+(6, '大腸麵線', 2, 0),
+(20, '夜店', 5, 0),
+(21, '酒吧', 5, 0),
+(22, 'KTV', 5, 4),
+(23, '海邊', 5, 1),
+(24, '遊樂園', 5, 1),
+(25, '爬山', 5, 0),
+(26, '河濱公園', 5, 0),
+(27, '貓', 6, 0),
+(28, '狗', 6, 1),
+(29, '蜜袋鼯', 6, 0),
+(30, '龍貓', 6, 0),
+(31, '蜥蜴', 6, 0),
+(32, '鱷魚', 6, 0),
+(33, '刺蝟', 6, 0),
+(34, '螞蟻', 6, 0),
+(35, '難', 7, 0),
+(36, '不難', 7, 0),
+(37, '火龍果', 8, 0),
+(38, '奇異果', 8, 0),
+(39, '蘋果', 8, 0),
+(40, '葡萄', 8, 0),
+(41, '楊桃', 8, 0),
+(42, '柳丁', 8, 0),
+(43, '荔枝', 8, 0),
+(44, '龍眼', 8, 0),
+(45, '小米', 9, 0),
+(46, '蘋果', 9, 0),
+(47, '紅米', 9, 0),
+(48, 'OPPO', 9, 0),
+(49, 'realme', 9, 0),
+(50, '三星', 9, 0),
+(51, 'HTC', 9, 0),
+(52, 'SONY', 9, 0),
+(53, '華為', 9, 0),
+(54, 'vivo', 9, 0);
 
 -- --------------------------------------------------------
 
@@ -95,19 +144,33 @@ CREATE TABLE `subjects` (
 --
 
 INSERT INTO `subjects` (`id`, `subject`, `type_id`, `multiple`, `mulit_limit`, `start`, `end`, `total`) VALUES
-(1, '午餐吃什麼', 1, 0, 1, '2022-06-22', '2022-07-02', 0),
-(2, '今天早餐吃什麼', 1, 0, 1, '2022-06-22', '2022-07-02', 0);
+(2, '今天早餐吃什麼', 4, 0, 1, '2022-06-22', '2022-07-02', 2),
+(5, '假日去哪玩', 4, 0, 1, '2022-06-23', '2022-07-03', 6),
+(6, '有養什麼寵物呢?', 4, 1, 1, '2022-06-23', '2022-07-03', 1),
+(7, '今天課程難不難', 4, 0, 1, '2022-06-23', '2022-07-03', 0),
+(8, '喜歡吃什麼水果', 4, 1, 1, '2022-06-23', '2022-07-03', 0),
+(9, '喜歡的手機品牌', 6, 0, 1, '2022-06-29', '2022-07-09', 0);
 
 -- --------------------------------------------------------
 
 --
--- 資料表結構 `type`
+-- 資料表結構 `types`
 --
 
-CREATE TABLE `type` (
+CREATE TABLE `types` (
   `id` int(11) UNSIGNED NOT NULL COMMENT '序號',
   `name` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '分類名稱'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `types`
+--
+
+INSERT INTO `types` (`id`, `name`) VALUES
+(3, '政治'),
+(4, '生活'),
+(5, '經濟'),
+(6, '科技');
 
 -- --------------------------------------------------------
 
@@ -148,9 +211,9 @@ ALTER TABLE `admins`
   ADD PRIMARY KEY (`id`);
 
 --
--- 資料表索引 `log`
+-- 資料表索引 `logs`
 --
-ALTER TABLE `log`
+ALTER TABLE `logs`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -166,9 +229,9 @@ ALTER TABLE `subjects`
   ADD PRIMARY KEY (`id`);
 
 --
--- 資料表索引 `type`
+-- 資料表索引 `types`
 --
-ALTER TABLE `type`
+ALTER TABLE `types`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -188,28 +251,28 @@ ALTER TABLE `admins`
   MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '序號';
 
 --
--- 使用資料表自動遞增(AUTO_INCREMENT) `log`
+-- 使用資料表自動遞增(AUTO_INCREMENT) `logs`
 --
-ALTER TABLE `log`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '序號';
+ALTER TABLE `logs`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '序號', AUTO_INCREMENT=9;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `options`
 --
 ALTER TABLE `options`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '序號', AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '序號', AUTO_INCREMENT=55;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `subjects`
 --
 ALTER TABLE `subjects`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '序號', AUTO_INCREMENT=3;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '序號', AUTO_INCREMENT=10;
 
 --
--- 使用資料表自動遞增(AUTO_INCREMENT) `type`
+-- 使用資料表自動遞增(AUTO_INCREMENT) `types`
 --
-ALTER TABLE `type`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '序號';
+ALTER TABLE `types`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '序號', AUTO_INCREMENT=7;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `users`
