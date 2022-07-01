b012d5c627c437368bedd4af7a9d8158b4e1aef0
Hollie <tsubasababy1013@gmail.com>
2022-07-01 17:05:51 +0800

    debug這麼多次發現了原來bug是不小心把vote.sql裡面的passnote類型設成int導致的錯誤

--- add_member.php
+++ add_member.php
@@ -1,25 +1,12 @@
 <?php
-// include "connect.php"; //連線資料庫
-
-// $pw=md5($_POST['pw']);//把密碼用MP5顯示
-// $sql="INSERT INTO `users` (`acc`,`pw`,`name`,`birthday`,`addr`,`email`,`passnote`) 
-//                     values('{$_POST['acc']}','$pw','{$_POST['name']}','{$_POST['birthday']}','{$_POST['addr']}','{$_POST['email']}','{$_POST['passnote']}');";
-
-// $pdo->exec($sql);
-
-// header("location:login.php");
-
-?>
-
-<?php
 include "connect.php"; //連線資料庫
 
 $pw=md5($_POST['pw']);//把密碼用MP5顯示
 $sql="INSERT INTO `users` (`acc`,`pw`,`name`,`birthday`,`addr`,`email`,`passnote`) 
                     values('{$_POST['acc']}','$pw','{$_POST['name']}','{$_POST['birthday']}','{$_POST['addr']}','{$_POST['email']}','{$_POST['passnote']}');";
-echo '$sql='.$sql;
-// $pdo->exec($sql);
 
-// header("location:login.php");
+$pdo->exec($sql);
+
+header("location:login.php");
 
 ?>
