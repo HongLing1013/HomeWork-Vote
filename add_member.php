bf259bbdc560be9969c7b3c215effbf9cfdd50f3
Hollie <tsubasababy1013@gmail.com>
2022-07-01 16:51:13 +0800

    debug

--- add_member.php
+++ add_member.php
@@ -1,12 +1,25 @@
 <?php
+// include "connect.php"; //連線資料庫
+
+// $pw=md5($_POST['pw']);//把密碼用MP5顯示
+// $sql="INSERT INTO `users` (`acc`,`pw`,`name`,`birthday`,`addr`,`email`,`passnote`) 
+//                     values('{$_POST['acc']}','$pw','{$_POST['name']}','{$_POST['birthday']}','{$_POST['addr']}','{$_POST['email']}','{$_POST['passnote']}');";
+
+// $pdo->exec($sql);
+
+// header("location:login.php");
+
+?>
+
+<?php
 include "connect.php"; //連線資料庫
 
 $pw=md5($_POST['pw']);//把密碼用MP5顯示
 $sql="INSERT INTO `users` (`acc`,`pw`,`name`,`birthday`,`addr`,`email`,`passnote`) 
                     values('{$_POST['acc']}','$pw','{$_POST['name']}','{$_POST['birthday']}','{$_POST['addr']}','{$_POST['email']}','{$_POST['passnote']}');";
+echo '$sql='.$sql;
+// $pdo->exec($sql);
 
-$pdo->exec($sql);
-
-header("location:login.php");
+// header("location:login.php");
 
 ?>
