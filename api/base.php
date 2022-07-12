71a511cf13752229d22eaf5b79387f34e259e45a
unknown <tsubasababy1013@gmail.com>
2022-07-12 10:23:20 +0800

    try push

--- api/base.php
+++ api/base.php
@@ -2,19 +2,19 @@
 session_start();
 date_default_timezone_set('Asia/Taipei');
 
-$dsn = "mysql:host=localhost;charset=utf8;dbname=vote";
-$pdo=new PDO($dsn, 'root', '');
+// $dsn = "mysql:host=localhost;charset=utf8;dbname=vote";
+// $pdo=new PDO($dsn, 'root', '');
 
-// $dsn="mysql:host=localhost;charset=utf8;dbname=s1110208";
-// $pdo=new PDO($dsn,'s1110208','s1110208');
+$dsn="mysql:host=localhost;charset=utf8;dbname=s1110208";
+$pdo=new PDO($dsn,'s1110208','s1110208');
 
 function pdo(){
 
-    $dsn = "mysql:host=localhost;charset=utf8;dbname=vote";
-    return new PDO($dsn, 'root', '');
+    // $dsn = "mysql:host=localhost;charset=utf8;dbname=vote";
+    // return new PDO($dsn, 'root', '');
 
-    // $dsn="mysql:host=localhost;charset=utf8;dbname=s1110208";
-    // return new PDO($dsn,'s1110208','s1110208');
+    $dsn="mysql:host=localhost;charset=utf8;dbname=s1110208";
+    return new PDO($dsn,'s1110208','s1110208');
 }
 /**
  * $table - 資料表名稱 字串型式
